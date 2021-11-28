ccdf4203a39850ccf4e0a39a56e23351b5993a95
milenatteixeira <milenateixeira648@gmail.com>
2021-11-28 00:12:10 -0300

    dir and help instructions file added

---
+++ dir_help.py
@@ -0,0 +1,9 @@
+"""
+Utilitários:
+dir -> apresenta atributos/propriedades e funções/métodos disponíveis
+    para determinado tipo de dado ou variável
+
+help -> apresenta a documentação dos atributos/propriedades e funções/métodos
+    disponíveis determinado tipo de dado ou variável
+
+"""

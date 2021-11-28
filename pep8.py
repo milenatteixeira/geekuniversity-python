c54a429b21d1b6aac04e018860c30bacbc599b77
milenatteixeira <milenateixeira648@gmail.com>
2021-11-27 23:55:06 -0300

    few pep8 tips added

---
+++ pep8.py
@@ -0,0 +1,19 @@
+"""
+PEP8 - Python Enhancement Proposal
+
+Propostas de melhorias para a linguagem Python
+
+import this -> The Zen of Python by Tim Peters
+
+PEP8: escrever código python de forma pythônica
+
+[1] CamelCase para nome de Classes;
+[2] snake_case para nome de variáveis e funções;
+[3] 4 espaços para indentação; (não usar o tab)
+[4] Linhas em branco são importantes
+[5] Imports em linhas separadas
+    Imports devem ser colocados no inicio do arquivo, após comentários e docstrings, mas antes de código
+[6] Espaços mínimos em expressoes e instruções
+[7] Linha em branco após finalizar uma instrução
+
+"""

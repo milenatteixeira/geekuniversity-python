ca1ec7282ce649c0b285a92018b34c8410ec67e5
milenatteixeira <milenateixeira648@gmail.com>
2022-02-05 19:56:46 -0300

    added 1-3 exercicios from secao6 (loop structures)

---
+++ exercicios/secao6/1-10/2.py
@@ -0,0 +1,8 @@
+for num in range(1, 101):
+    print(num, end='')
+
+print()
+num = 1
+while num <= 100:
+    print(num, end='')
+    num += 1
---
+++ exercicios/secao6/1-10/3.py
@@ -0,0 +1,8 @@
+from time import sleep
+
+num = 10
+while num >= 0:
+    print(num)
+    sleep(1)
+    num -= 1
+print('FIM')
